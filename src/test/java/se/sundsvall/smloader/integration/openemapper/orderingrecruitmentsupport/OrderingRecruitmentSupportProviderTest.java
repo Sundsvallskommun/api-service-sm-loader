5733839271bb838a49f1bf7f5bd74aaf83ae4a40
Michael Wallner <Michael.Wallner@sundsvall.se>
2025-06-26 11:54:04 +0200

    DRAKEN-1624: Fix for numberOfPositions and endDate

--- src/test/java/se/sundsvall/smloader/integration/openemapper/orderingrecruitmentsupport/OrderingRecruitmentSupportProviderTest.java
+++ src/test/java/se/sundsvall/smloader/integration/openemapper/orderingrecruitmentsupport/OrderingRecruitmentSupportProviderTest.java
@@ -24,6 +24,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -46,8 +47,11 @@ class OrderingRecruitmentSupportProviderTest {
 		assertThat(provider.getSupportedFamilyId()).isEqualTo("123");
 	}
 
-	@Test
-	void mapToErrand() throws Exception {
+	@ParameterizedTest
+	@ValueSource(strings = {
+		"flow-instance-rekryteringsstod.xml", "flow-instance-rekryteringsstod-tidsbegransad.xml"
+	})
+	void mapToErrand(String fileName) throws Exception {
 
 		final var priority = "MEDIUM";
 		final var category = "category";
@@ -57,7 +61,7 @@ class OrderingRecruitmentSupportProviderTest {
 		when(properties.getCategory()).thenReturn(category);
 		when(properties.getType()).thenReturn(type);
 
-		final var stringBytes = readOpenEFile("flow-instance-rekryteringsstod.xml");
+		final var stringBytes = readOpenEFile(fileName);
 
 		final var errand = provider.mapToErrand(stringBytes);
 
@@ -68,40 +72,78 @@ class OrderingRecruitmentSupportProviderTest {
 		assertThat(errand.getClassification()).isEqualTo(new Classification().category("PARTIAL_PACKAGE").type("PARTIAL_PACKAGE.EMPLOYEE"));
 		assertThat(errand.getLabels()).isEqualTo(List.of("PARTIAL_PACKAGE", "PARTIAL_PACKAGE.EMPLOYEE"));
 		assertThat(errand.getBusinessRelated()).isFalse();
-		assertThat(errand.getParameters()).hasSize(33).extracting(Parameter::getKey, Parameter::getValues, Parameter::getDisplayName, Parameter::getGroup).containsExactly(
-			tuple("municipalityOrCompany", List.of("Kommunal förvaltning"), "Gäller kommunal förvaltning eller bolag och förbund?", null),
-			tuple("department", List.of("Kommunstyrelsekontoret"), "Organisation", null),
-			tuple("referenceNumber", List.of("007007007 - Bond James"), "Referensnummer för mottagare av fakturan", null),
-			tuple("securityClassification", List.of("Ja"), "Behöver tjänsten säkerhetsklassas?", null),
-			tuple("orderDetails", List.of("Delpaket"), "Beställning", null),
-			tuple("position", List.of("Medarbetare"), "Tjänst", null),
-			tuple("readyProfile", List.of("Jag har en färdig annons"), "Färdig annons eller stöd i annonsutformning", null),
-			tuple("workplaceTitle", List.of("Spion"), "Tjänstetitel", null),
-			tuple("workplace", List.of("MI6 Headquarters"), "Arbetsplats", null),
-			tuple("employmentType", List.of("Tillsvidareanställning"), "Anställningsform", null),
-			tuple("startDate", List.of("2025-04-24"), "Tillträde", null),
-			tuple("scope", List.of("100"), "Omfattning i procent", null),
-			tuple("numberOfPositions", List.of("1"), "Antal tjänster", null),
-			tuple("recruitmentAccess", List.of("Miss Moneypenny|miss.moneypenny@mi6.se|Sekreterare"), "Namn|E-Post|Roll", null),
-			tuple("workplaceInfo", List.of("Arbete över hela världen"), "Om arbetsplatsen", null),
-			tuple("jobTask", List.of("Insamling av information"), "Arbetsuppgifter", null),
-			tuple("qualifications", List.of("Stor och stark"), "Kvalifikationer", null),
-			tuple("advertising", List.of("Intern och extern"), "Annonsering", null),
-			tuple("advertisementContacts", List.of("M|Chef|060-666666"), "Namn|Title|Telefonnummer", null),
-			tuple("unionContacts", List.of("Ulla Ullasson|städförbundet|010-1234567"), "Namn|Fackförbund|Telefonnummer", null),
-			tuple("addSelectionQuestions", List.of("Ja"), "Vill du lägga till urvalsfrågor? (timdebitering)", null),
-			tuple("question1", List.of("Hur långt är ett snöre?"), "Fråga 1", null),
-			tuple("question2", List.of("Vad är klockan?"), "Fråga 2", null),
-			tuple("question3", List.of("Finns det tårta?"), "Fråga 3", null),
-			tuple("advertisementType", List.of("Intern och extern"), "Annonsering", null),
-			tuple("advertisementTime", List.of("2 Veckor"), "Annonseringtid", null),
-			tuple("additionalSupport", List.of("Utökad annonsering (sociala medier)", "Tester"), "Önskar stöd i rekrytering", null),
-			tuple("mediaChoices", List.of("Målgruppsstyrda annonser (sociala medier)"), "Mediaval", null),
-			tuple("advertisementPackage", List.of("Large"), "Annonspaket", null),
-			tuple("testType", List.of("Personlighetstest"), "Beställda tester", null),
-			tuple("inDepthInterview", List.of("Ja"), "Fördjupad intervju med kandidater beställd", null),
-			tuple("recruitmentSupport", List.of("Okej"), "Konsultativt rekryteringsstöd per timme", null),
-			tuple("additionalInformation", List.of("Lycka till!"), "Övrig information", null));
+		if (fileName.equals("flow-instance-rekryteringsstod.xml")) {
+			assertThat(errand.getParameters()).hasSize(33).extracting(Parameter::getKey, Parameter::getValues, Parameter::getDisplayName, Parameter::getGroup).containsExactly(
+				tuple("municipalityOrCompany", List.of("Kommunal förvaltning"), "Gäller kommunal förvaltning eller bolag och förbund?", null),
+				tuple("department", List.of("Kommunstyrelsekontoret"), "Organisation", null),
+				tuple("referenceNumber", List.of("007007007 - Bond James"), "Referensnummer för mottagare av fakturan", null),
+				tuple("securityClassification", List.of("Ja"), "Behöver tjänsten säkerhetsklassas?", null),
+				tuple("orderDetails", List.of("Delpaket"), "Beställning", null),
+				tuple("position", List.of("Medarbetare"), "Tjänst", null),
+				tuple("readyProfile", List.of("Jag har en färdig annons"), "Färdig annons eller stöd i annonsutformning", null),
+				tuple("workplaceTitle", List.of("Spion"), "Tjänstetitel", null),
+				tuple("workplace", List.of("MI6 Headquarters"), "Arbetsplats", null),
+				tuple("employmentType", List.of("Tillsvidareanställning"), "Anställningsform", null),
+				tuple("startDate", List.of("2025-04-24"), "Tillträde", null),
+				tuple("scope", List.of("100"), "Omfattning i procent", null),
+				tuple("numberOfPositions", List.of("1"), "Antal tjänster", null),
+				tuple("recruitmentAccess", List.of("Miss Moneypenny|miss.moneypenny@mi6.se|Sekreterare"), "Namn|E-Post|Roll", null),
+				tuple("workplaceInfo", List.of("Arbete över hela världen"), "Om arbetsplatsen", null),
+				tuple("jobTask", List.of("Insamling av information"), "Arbetsuppgifter", null),
+				tuple("qualifications", List.of("Stor och stark"), "Kvalifikationer", null),
+				tuple("advertising", List.of("Intern och extern"), "Annonsering", null),
+				tuple("advertisementContacts", List.of("M|Chef|060-666666"), "Namn|Title|Telefonnummer", null),
+				tuple("unionContacts", List.of("Ulla Ullasson|städförbundet|010-1234567"), "Namn|Fackförbund|Telefonnummer", null),
+				tuple("addSelectionQuestions", List.of("Ja"), "Vill du lägga till urvalsfrågor? (timdebitering)", null),
+				tuple("question1", List.of("Hur långt är ett snöre?"), "Fråga 1", null),
+				tuple("question2", List.of("Vad är klockan?"), "Fråga 2", null),
+				tuple("question3", List.of("Finns det tårta?"), "Fråga 3", null),
+				tuple("advertisementType", List.of("Intern och extern"), "Annonsering", null),
+				tuple("advertisementTime", List.of("2 Veckor"), "Annonseringtid", null),
+				tuple("additionalSupport", List.of("Utökad annonsering (sociala medier)", "Tester"), "Önskar stöd i rekrytering", null),
+				tuple("mediaChoices", List.of("Målgruppsstyrda annonser (sociala medier)"), "Mediaval", null),
+				tuple("advertisementPackage", List.of("Large"), "Annonspaket", null),
+				tuple("testType", List.of("Personlighetstest"), "Beställda tester", null),
+				tuple("inDepthInterview", List.of("Ja"), "Fördjupad intervju med kandidater beställd", null),
+				tuple("recruitmentSupport", List.of("Okej"), "Konsultativt rekryteringsstöd per timme", null),
+				tuple("additionalInformation", List.of("Lycka till!"), "Övrig information", null));
+		} else {
+			assertThat(errand.getParameters()).hasSize(34).extracting(Parameter::getKey, Parameter::getValues, Parameter::getDisplayName, Parameter::getGroup).containsExactly(
+				tuple("municipalityOrCompany", List.of("Kommunal förvaltning"), "Gäller kommunal förvaltning eller bolag och förbund?", null),
+				tuple("department", List.of("Kommunstyrelsekontoret"), "Organisation", null),
+				tuple("referenceNumber", List.of("007007007 - Bond James"), "Referensnummer för mottagare av fakturan", null),
+				tuple("securityClassification", List.of("Ja"), "Behöver tjänsten säkerhetsklassas?", null),
+				tuple("orderDetails", List.of("Delpaket"), "Beställning", null),
+				tuple("position", List.of("Medarbetare"), "Tjänst", null),
+				tuple("readyProfile", List.of("Jag har en färdig annons"), "Färdig annons eller stöd i annonsutformning", null),
+				tuple("workplaceTitle", List.of("Spion"), "Tjänstetitel", null),
+				tuple("workplace", List.of("MI6 Headquarters"), "Arbetsplats", null),
+				tuple("employmentType", List.of("Tidsbegränsad anställning"), "Anställningsform", null),
+				tuple("startDate", List.of("2025-06-11"), "Tillträde", null),
+				tuple("endDate", List.of("2025-06-12"), "Slutdatum", null),
+				tuple("scope", List.of("100"), "Omfattning i procent", null),
+				tuple("numberOfPositions", List.of("1"), "Antal tjänster", null),
+				tuple("recruitmentAccess", List.of("Miss Moneypenny|miss.moneypenny@mi6.se|Sekreterare"), "Namn|E-Post|Roll", null),
+				tuple("workplaceInfo", List.of("Arbete över hela världen"), "Om arbetsplatsen", null),
+				tuple("jobTask", List.of("Insamling av information"), "Arbetsuppgifter", null),
+				tuple("qualifications", List.of("Stor och stark"), "Kvalifikationer", null),
+				tuple("advertising", List.of("Intern och extern"), "Annonsering", null),
+				tuple("advertisementContacts", List.of("M|Chef|060-666666"), "Namn|Title|Telefonnummer", null),
+				tuple("unionContacts", List.of("Ulla Ullasson|städförbundet|010-1234567"), "Namn|Fackförbund|Telefonnummer", null),
+				tuple("addSelectionQuestions", List.of("Ja"), "Vill du lägga till urvalsfrågor? (timdebitering)", null),
+				tuple("question1", List.of("Hur långt är ett snöre?"), "Fråga 1", null),
+				tuple("question2", List.of("Vad är klockan?"), "Fråga 2", null),
+				tuple("question3", List.of("Finns det tårta?"), "Fråga 3", null),
+				tuple("advertisementType", List.of("Intern och extern"), "Annonsering", null),
+				tuple("advertisementTime", List.of("2 Veckor"), "Annonseringtid", null),
+				tuple("additionalSupport", List.of("Utökad annonsering (sociala medier)", "Tester"), "Önskar stöd i rekrytering", null),
+				tuple("mediaChoices", List.of("Målgruppsstyrda annonser (sociala medier)"), "Mediaval", null),
+				tuple("advertisementPackage", List.of("Large"), "Annonspaket", null),
+				tuple("testType", List.of("Personlighetstest"), "Beställda tester", null),
+				tuple("inDepthInterview", List.of("Ja"), "Fördjupad intervju med kandidater beställd", null),
+				tuple("recruitmentSupport", List.of("Okej"), "Konsultativt rekryteringsstöd per timme", null),
+				tuple("additionalInformation", List.of("Lycka till!"), "Övrig information", null));
+		}
 		assertThat(errand.getStakeholders()).hasSize(2).extracting(
 			Stakeholder::getRole,
 			Stakeholder::getFirstName,
